b5a0e79701ff10dbc32ec35678fa2172683b1805
Dominik Sidorczuk <139627345+Dominik-Sidorczuk@users.noreply.github.com>
2024-09-11 21:07:47 +0200

    Update BME680.ino

--- IoT-Home Assistant/BME680/BME680.ino
+++ IoT-Home Assistant/BME680/BME680.ino
@@ -17,20 +17,27 @@ const int mqttPort = 1883;                     // Standardowy port MQTT
 const char* mqttUser = "XXXXXXXXX";              // Nazwa użytkownika MQTT (opcjonalne)
 const char* mqttPassword = "XXXXXXXX";       // Hasło użytkownika MQTT (opcjonalne)
 
-WiFiClient espClient;                          // Obiekt klienta WiFi
-PubSubClient client(espClient);                // Obiekt klienta MQTT
-Bsec iaqSensor;                                // Obiekt czujnika BME680
+AsyncMqttClient mqttClient;                      // Obiekt klienta MQTT
+Bsec iaqSensor;                                        // Obiekt czujnika BME680
+Adafruit_SHT4x sht4 = Adafruit_SHT4x();                // Obiekt czujnika SHT41
+Adafruit_VEML7700 veml = Adafruit_VEML7700();
+
+
+// Funkcje callback MQTT
+void connectToMqtt();   
+void onMqttConnect(bool sessionPresent);
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason);
+bool publishMessage(const char* topic, const char* payload);
 
 // Dane dla wstępnej kalibracji BSEC
 const uint8_t bsec_config_iaq[] = {
-#include "config/generic_33v_3s_28d/bsec_iaq.txt"  // Plik konfiguracyjny BSEC
+#include "config/generic_33v_3s_28d/bsec_iaq.txt"      // Plik konfiguracyjny BSEC
 };
 
-// Stałe definiujące częstotliwość zapisu stanu BSEC i wysyłania discovery do Home Assistant
-#define STATE_SAVE_PERIOD   UINT32_C(360 * 60 * 1000) // 360 minut - zapis stanu
-#define DISCOVERY_INTERVAL 720 * 60 * 1000           // 720 minut - discovery
+#define STATE_SAVE_PERIOD   UINT32_C(360 * 60 * 1000)  // 360 minut częstotliwość zapis stanu BME680
+#define DISCOVERY_INTERVAL 720 * 60 * 1000             // 720 minut w milisekundach, częstotliwość wystłania wiadomości HomeAsistant Discovery
 
-// Lista sensorów BME680 dla BSEC
+// Listy sensorów BME680 BSEC
 bsec_virtual_sensor_t sensorList[7] = {
 BSEC_OUTPUT_STATIC_IAQ,
 BSEC_OUTPUT_RAW_PRESSURE,
@@ -41,6 +48,7 @@ BSEC_OUTPUT_RUN_IN_STATUS,
 BSEC_OUTPUT_IAQ
 };
 
+// Zmienne czasowe
 uint8_t bsecState[BSEC_MAX_STATE_BLOB_SIZE] = {0};
 uint16_t stateUpdateCounter = 0;
 uint32_t time_ms = millis();
@@ -48,7 +56,7 @@ uint32_t last_time_ms = 0;
 uint32_t last_publish_time_ms = 0; 
 unsigned long lastDiscoveryTime = 0;
 
-// Unikalne identyfikatory urządzenia
+// Unikalny identyfikator urządzenia
 String macAddress; 
 String deviceId; 
 String stateTopic;
@@ -56,7 +64,7 @@ String configTopic;
 String discoveryPrefix = "homeassistant";
 String output;
 
-// Tablica sensorów do discovery Home Assistant
+// Tablica sensorów do discovery
 struct SensorInfo {
     const char* name;
     const char* unit;
@@ -65,125 +73,17 @@ struct SensorInfo {
     bool is_available;  // Zmienna do śledzenia dostępności sensora
 };
 
-// Informacje o sensorach dla Home Assistant
+// Informacje o sensorach dla HomneAssistant
 SensorInfo sensors[] = {
     {"temperature", "°C", "temperature", "Temperature", false},
     {"humidity", "%", "humidity", "Humidity", false},
     {"pressure", "hPa", "pressure", "Pressure", false},
     {"static_iaq", "IAQ", "aqi", "Static IAQ", false},
-    {"iaqAccuracy", "none", "none", "IAQ Accuracy", false}, 
+    {"iaqAccuracy", "none", "none", "IAQ Accuracy", false},
+    {"illuminance", "lx", "illuminance", "Illuminance", false}, 
 };
 
-// Typ wyliczeniowy dla tematów MQTT
-enum MqttTopic {
-    PUBREC = 0,
-    PUBREL,
-    PUBCOMP,
-    PUBACK
-};
-// Tablica do śledzenia wiadomości QoS
-const char* topicNames[] = {
-    "bme680/pubrec",
-    "bme680/pubrel",
-    "bme680/pubcomp",
-    "bme680/puback"
-};
-
-// Tablica do śledzenia stanu wiadomości
-struct MessageStatus {
-    uint16_t packetId;
-    bool delivered;
-};
-
-// Tablica do przechowywania statusu wiadomości (zwiększ rozmiar w razie potrzeby)
-MessageStatus messageStatuses[10];
-int numMessages = 0; // Liczba wiadomości w tablicy
-
-
-// Funkcja do ponownego łączenia z MQTT w razie utraty połączenia
-bool mqttConnect() {
-    int retries = 0;
-    const int maxRetries = 20;
-    uint32_t retryDelay = 5000; // Czas po jakim nastąpi ponowna próba w ms
-    int wifiReconnectCount = 0; // Liczba prób połączenia do wifi
-
-    while (!client.connected() && retries < maxRetries) {
-        Serial.print("Próba połączenia MQTT (" + String(retries + 1) + "/" + String(maxRetries) + ")...");
-
-        if (client.connect("ESP32Client", mqttUser, mqttPassword)) {
-            Serial.println("Połączono!");
-            return true;
-        } else {
-            Serial.print("Niepowodzenie, rc=");
-            Serial.print(client.state());
-            Serial.println(" (WiFi stan: " + String(WiFi.status()) + ")");
-
-            retries++;
-            retryDelay *= 2; 
-
-            // Check if the issue is with WiFi
-            if (WiFi.status() != WL_CONNECTED) {
-                wifiReconnectCount++;
-                if (wifiReconnectCount > 5) {
-                    Serial.println("Zbyt wiele nieudanych prób połączenia Wi-Fi. Ponowne uruchamianie ESP32...");
-                    ESP.restart(); // Restart ESP32 jeżeli się nie powiodło WiFi
-                } else {
-                    Serial.println("Próba ponownego połączenia z Wi-Fi...");
-                    WiFi.reconnect();
-                }
-            }
-
-            delay(retryDelay);
-        }
-    }
-
-    Serial.println("Nie udało się połączyć z MQTT.");
-    return false;
-}
-
-// Callback dla odbieranych wiadomości MQTT
-void mqttCallback(char* topic, byte* payload, unsigned int length) {
-    Serial.print("Odebrano wiadomość [");
-    Serial.print(topic);
-    Serial.print("] ");
-    for (int i = 0; i < length; i++) {
-        Serial.print((char)payload[i]);
-    }
-    Serial.println();
-
-    // Obsługa potwierdzeń QoS 1 (PUBACK)
-    if (length == 2) {
-        uint16_t packetId = (payload[0] << 8) | payload[1];
-        for (int i = 0; i < numMessages; i++) {
-            if (messageStatuses[i].packetId == packetId) {
-                if (String(topic) == topicNames[PUBACK]) {
-                    Serial.println("Potwierdzenie PUBACK odebrane (packetId: " + String(packetId) + ")");
-                    messageStatuses[i].delivered = true;
-                    break; 
-                }
-            }
-        }
-    }
-}
-
-// Funkcja do publikowania wiadomości z QoS
-bool publishMessage(const char* topic, const char* payload, uint8_t qos = 0) {
-    if (!client.connected()) {
-        if (!mqttConnect()) {
-            return false;
-        }
-    }
-
-    uint16_t packetId = client.publish(topic, payload, qos);
-    if (qos == 1) {
-        messageStatuses[numMessages].packetId = packetId;
-        messageStatuses[numMessages].delivered = false;
-        numMessages++;
-    }
-    return (packetId > 0); 
-}
-
-// Klasa SensorData 
+// Klasa SensorData (z uśrednianiem i JSON)
 class SensorData {
 public:
     float temperature = 0.0;
@@ -191,13 +91,15 @@ public:
     float pressure = 0.0;
     int staticIaq = 0;
     int iaqAccuracy = 0;
+    float illuminance = 0.0;
 
-    // Obiekty RunningAverage
+    // Obiekty RunningAverage (wewnątrz klasy)
     RunningAverage temperatureAverage = RunningAverage(10);
     RunningAverage humidityAverage = RunningAverage(10);
     RunningAverage pressureAverage = RunningAverage(10);
     RunningAverage accuracyAverage = RunningAverage(10);
     RunningAverage staticIaqAverage = RunningAverage(10);
+    RunningAverage illuminanceAverage = RunningAverage(10);
 
     // Metoda aktualizująca dane i uśredniająca
     void updateData(Bsec& iaqSensor) {
@@ -205,17 +107,35 @@ public:
             staticIaq = iaqSensor.staticIaq;
             temperature = iaqSensor.temperature;
             humidity = iaqSensor.humidity;
-            pressure = iaqSensor.pressure / 100.0f; // Zmiana jednostek do hPa
+            pressure = iaqSensor.pressure / 100.0f; // Normalizacja ciśnienia
             iaqAccuracy = iaqSensor.iaqAccuracy;
+            illuminance = veml.readLux(VEML_LUX_AUTO);
 
-            temperatureAverage.addValue(temperature);
-            humidityAverage.addValue(humidity);
+            // Odczyt danych z SHT41
+            sensors_event_t humidityData, tempData;
+            sht4.getEvent(&humidityData, &tempData);
+
+            // Uśrednianie temperatury (ważone)
+            const float BME680_TEMP_WEIGHT = 0.4; // Waga dla BME680
+            const float SHT41_TEMP_WEIGHT = 0.6;  // Waga dla SHT41
+            float averagedTemperature = (temperature * BME680_TEMP_WEIGHT) + (tempData.temperature * SHT41_TEMP_WEIGHT);
+
+            // Uśrednianie wilgotności (ważone)
+            const float BME680_HUMIDITY_WEIGHT = 0.4; // Waga dla BME680
+            const float SHT41_HUMIDITY_WEIGHT = 0.6;  // Waga dla SHT41
+            float averagedHumidity = (humidity * BME680_HUMIDITY_WEIGHT) + (humidityData.relative_humidity * SHT41_HUMIDITY_WEIGHT);
+
+            // Dodaj uśrednione wartości do RunningAverage
+            temperatureAverage.addValue(averagedTemperature);
+            humidityAverage.addValue(averagedHumidity);
             pressureAverage.addValue(pressure);
             accuracyAverage.addValue(iaqAccuracy);
             staticIaqAverage.addValue(staticIaq);
+            illuminanceAverage.addValue(illuminance);
         }
     }
-    // Zwracanie JSON z uśrednionymi danymi dla 10 pomiarów. 
+
+    // Metoda zwracająca JSON z uśrednionymi danymi
     String getAveragedDataAsJSON() {
         if (temperatureAverage.getCount() == 10) {
             String jsonOutput = "{";
@@ -223,14 +143,17 @@ public:
             jsonOutput += "\"humidity\":" + String(humidityAverage.getAverage(), 2) + ",";
             jsonOutput += "\"pressure\":" + String(pressureAverage.getAverage(), 2) + ",";
             jsonOutput += "\"iaqAccuracy\":" + String(accuracyAverage.getAverage(), 0) + ",";
-            jsonOutput += "\"static_iaq\":" + String(staticIaqAverage.getAverage(), 0);
+            jsonOutput += "\"static_iaq\":" + String(staticIaqAverage.getAverage(), 0)+ ",";;
+            jsonOutput += "\"illuminance\":" + String(illuminanceAverage.getAverage(), 1);
             jsonOutput += "}";
 
+            // Wyczyść obiekty RunningAverage po użyciu
             temperatureAverage.clear();
             humidityAverage.clear();
             pressureAverage.clear();
             accuracyAverage.clear();
             staticIaqAverage.clear();
+            illuminanceAverage.clear();
 
             return jsonOutput;
         } else {
@@ -238,26 +161,74 @@ public:
         }
     }
 };
+SensorData sensorData; // Utwórz obiekt klasy
 
-SensorData sensorData; // obiekt klasy
+// Dodaj funkcję getSensorIndex()
+int getSensorIndex(const char* sensorName) {
+  for (int i = 0; i < sizeof(sensors) / sizeof(sensors[0]); i++) {
+    if (strcmp(sensors[i].name, sensorName) == 0) {
+      return i;
+    }
+  }
+  return -1; // Sensor not found
+}
+
+// Funkcja do obsługi połączenia MQTT
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+// Callback dla zdarzenia połączenia MQTT
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}
+
+// Callback dla zdarzenia rozłączenia MQTT
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    connectToMqtt(); 
+  }
+}
+
+// Callback dla nieudanej publikacji wiadomości MQTT
+void onMqttPublish(uint16_t packetId) {
+  Serial.println("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+// Funkcja do publikowania wiadomości (uproszczona)
+bool publishMessage(const char* topic, const char* payload) {
+  if (!mqttClient.connected()) {
+    return false;
+  }
+  // Publish the message (QoS 0 by default)
+  mqttClient.publish(topic, 0, false, payload); 
+  return true;
+}
 
 // Funkcja do wysyłania discovery dla Home Assistant
 void MqttHomeAssistantDiscovery(const char* sensorName, const char* unit, const char* friendlyName, const char* deviceClass) {
     // Sprawdź, czy klient MQTT jest połączony
-    if (client.connected()) {
+    if (mqttClient.connected()) {
         Serial.println("Wysyłanie wiadomości discovery do Home Assistant...");
 
         String configTopic = discoveryPrefix + "/sensor/" + deviceId + "/" + sensorName + "/config";
 
         // Tworzenie payloadu discovery jako obiektu JSON
-        DynamicJsonDocument payload(1024); // Duuuży rozmiar bufora JSON dla bezpieczeństwa
+        DynamicJsonDocument payload(1024); // Zwiększony rozmiar bufora JSON dla bezpieczeństwa
 
         payload["name"] = friendlyName;
         payload["unique_id"] = deviceId + "_" + sensorName;
         payload["state_topic"] = stateTopic;
         payload["value_template"] = "{{ value_json." + String(sensorName) + " }}";
 
-        // Ustawienie klasy urządzenia i jednostki 
+        // Ustawienie klasy urządzenia i jednostki miary (jeśli dotyczy)
         if (String(deviceClass) != "none") {
             payload["device_class"] = deviceClass;
         }
@@ -267,10 +238,10 @@ void MqttHomeAssistantDiscovery(const char* sensorName, const char* unit, const 
 
         // Informacje o urządzeniu (wspólne dla wszystkich sensorów)
         JsonObject device = payload.createNestedObject("device");
-        device["name"] = "ESP32 BME680";
+        device["name"] = "Stacja 1 - " + deviceId;
         JsonArray identifiers = device.createNestedArray("identifiers");
         identifiers.add(deviceId);
-        device["model"] = "BME680";
+        device["model"] = "BME680 i SHT41";
         device["suggested_area"] = "Pokój";
         device["manufacturer"] = "Dominik Sidorczuk";
 
@@ -279,11 +250,11 @@ void MqttHomeAssistantDiscovery(const char* sensorName, const char* unit, const 
         serializeJson(payload, discoveryMessage);
 
         // Publikacja wiadomości discovery z QoS 1
-        uint16_t packetId = client.publish(configTopic.c_str(), discoveryMessage.c_str(), true);
+        uint16_t packetId = mqttClient.publish(configTopic.c_str(), 0, true, discoveryMessage.c_str());
 
         if (packetId) {
             Serial.println("Wiadomość discovery wysłana pomyślnie (packetId: " + String(packetId) + ").");
-            
+            // Możesz dodać tutaj logikę śledzenia potwierdzeń (PUBACK), jeśli potrzebujesz
         } else {
             Serial.println("Błąd wysyłania wiadomości discovery.");
         }
@@ -293,53 +264,85 @@ void MqttHomeAssistantDiscovery(const char* sensorName, const char* unit, const 
 }
 
 void setup() {
-    EEPROM.begin(BSEC_MAX_STATE_BLOB_SIZE + 1);
-    Serial.begin(115200);
-    Wire.begin();
+  EEPROM.begin(BSEC_MAX_STATE_BLOB_SIZE + 1); // Inicjalizacja EEPROM
+  Serial.begin(115200);                       // Inicjalizacja portu szeregowego
+  Wire.begin();                               // Inicjalizacja magistrali I2C
 
-    delay(500);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
-        Serial.println("Łączenie z WiFi...");
-    }
-    Serial.println("Połączono z WiFi: " + WiFi.localIP().toString());
+  delay(500);  // Krótkie opóźnienie, aby dać czas na stabilizację
 
-    // Pobranie adresu MAC WiFi
-    macAddress = WiFi.macAddress();
-    macAddress.replace(":", "");
-    deviceId = "bme680_" + macAddress;
-    stateTopic = discoveryPrefix + "/sensor/" + deviceId + "/state";
-    configTopic = discoveryPrefix + "/sensor/" + deviceId + "/";
-    client.setServer(mqttServer, mqttPort);
-    client.setCallback(mqttCallback);
-    client.setBufferSize(896);
+  // Inicjalizacja WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi: " + WiFi.localIP().toString());
 
-    Serial.println("Adres MAC: " + macAddress);
-    Serial.println("Device ID: " + deviceId);
+  // Pobranie adresu MAC WiFi i usunięcie dwukropków
+  macAddress = WiFi.macAddress();
+  macAddress.replace(":", "");
+  deviceId = "Stacja_1_" + macAddress;
+  stateTopic = discoveryPrefix + "/sensor/" + deviceId + "/state";
+  configTopic = discoveryPrefix + "/sensor/" + deviceId + "/";
 
-      if (!mqttConnect()) {
-        ESP.restart(); 
-    } else {
-        // Wywołanie funkcji Disocvery
-        for (const auto& sensor : sensors) {
-            MqttHomeAssistantDiscovery(sensor.name, sensor.unit, sensor.friendlyName, sensor.deviceClass);  
-        }
-    }
+  // Konfiguracja MQTT
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(mqttServer, mqttPort);
+  mqttClient.setCredentials(mqttUser, mqttPassword);
 
-    delay(200);
-    iaqSensor.begin(BME68X_I2C_ADDR_HIGH, Wire);
-    delay(200);
-    checkIaqSensorStatus();
-   
-    iaqSensor.setConfig(bsec_config_iaq);
-    checkIaqSensorStatus();
-    loadState();
-    delay(200);
+  connectToMqtt();                               // Nawiąż połączenie MQTT
 
+  Serial.println("Adres MAC: " + macAddress);
+  Serial.println("Device ID: " + deviceId);
+
+  while (!Serial)
+  delay(500);  
+  // Inicjalizacja czujnika SHT41
+  if (!sht4.begin()) {
+    Serial.println("Couldn't find SHT4x");
+    while (1);
+  }
+
+  Serial.println("Found SHT4x sensor");
+  sht4.setPrecision(SHT4X_HIGH_PRECISION);
+  sht4.setHeater(SHT4X_NO_HEATER);
+  
+  delay(250);
+  // Initialize VEML7700 sensor
+  if (!veml.begin()) {
+    Serial.println("Couldn't find VEML7700 sensor!");
+    while (1); // Halt execution if sensor not found
+  }
+  Serial.println("Found VEML7700 sensor");
+
+
+  delay(250);
+  iaqSensor.begin(BME68X_I2C_ADDR_HIGH, Wire);  // Użyj BME68X_I2C_ADDR_PRIMARY lub BME68X_I2C_ADDR_SECONDARY w zależności od konfiguracji
+  checkIaqSensorStatus();
+  delay(250);
+  iaqSensor.setConfig(bsec_config_iaq);
+  checkIaqSensorStatus();
+  loadState();                                  // Załaduj stan BSEC z EEPROM
+  delay(250); 
 
   iaqSensor.updateSubscription(sensorList, 7, BSEC_SAMPLE_RATE_LP);
   checkIaqSensorStatus();
+  
+  // Inicjalizacja czujnika SHT41
+  if (!sht4.begin()) {
+    Serial.println("Couldn't find SHT4x");
+    while (1) delay(1);
+  }
+  Serial.println("Found SHT4x sensor");
+  sht4.setPrecision(SHT4X_HIGH_PRECISION);
+  sht4.setHeater(SHT4X_NO_HEATER);
+  
+  // Home Assistant Discovery
+  for (const auto& sensor : sensors) {
+    MqttHomeAssistantDiscovery(sensor.name, sensor.unit, sensor.friendlyName, sensor.deviceClass);
+  }
 }
 
 void loop() {
@@ -348,14 +351,15 @@ void loop() {
 
     String averagedJson = sensorData.getAveragedDataAsJSON();
     if (averagedJson != "") {
-        // Publikowanie uśrednionych danych przez MQTT jeśli są dostępne
-        if (publishMessage(stateTopic.c_str(), averagedJson.c_str(), 0)) {
-            Serial.println("Wartość przekazane do brokera MQTT: " + averagedJson);
+        // Publikowanie uśrednionych danych przez MQTT (jeśli są dostępne)
+        if (!mqttClient.publish(stateTopic.c_str(), 0, false, averagedJson.c_str())) {
+            Serial.println("Published state: " + averagedJson);
         } else {
-            Serial.println("Nie przekazano wiadomości MQTT");
+            Serial.println("Failed to publish state.");
         }
     }
 }
+
 
 /// Cuda od Bosha pod bliotekę BSEC nie dotykać bo rzeczywistość się zjebie !!!
 void checkIaqSensorStatus(void){
